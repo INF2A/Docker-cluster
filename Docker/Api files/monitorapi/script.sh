33bc88c4374697b753a393e483ac2784a38a3ff1
ericrutgers <eric.rutgers@protonmail.com>
2017-06-15 12:08:51 +0200

    Create script.sh

--- Docker/Api files/monitorapi/script.sh
+++ Docker/Api files/monitorapi/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $(docker node ps "$1" | grep 'Running' | egrep -o 'timeapi:|radioapi:|weatherapi:|newsapi:|helloworldapi:|caledarapi:')     
+echo $(docker node ps "$1" | grep 'Running' | egrep -o 'timeapi:|radioapi:|weatherapi:|newsapi:|helloworldapi:|calendarapi:')     
